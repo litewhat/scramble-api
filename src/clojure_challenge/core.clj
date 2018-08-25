959c4e69d233894b79f57b92954dac9128dca313
Paweł Zielonka <lite.what@gmail.com>
2018-08-25 21:10:48 +0200

    'clojure-challenge.core' refactor
    Removed unused imports

--- src/clojure_challenge/core.clj
+++ src/clojure_challenge/core.clj
@@ -1,21 +1,20 @@
-(ns clojure-challenge.core
-  (:require [clojure.math.combinatorics :as combo]))
+(ns clojure-challenge.core)
+
 
 ;; Clojure Challenge
 
+(defn- gte-and-not-nil? [x y]
+  "Returns true if x is not nil and is greater than y."
+  (if x (>= x y) false))
+
 (defn scramble?
-  [letters word]
-  (let [req-chars (set word)
-        init      (for [char-seq [letters word]]
-                    (map (frequencies char-seq) req-chars))
-        pred-fn   (fn [x y]
-                    (if x
-                      (>= x y)
-                      false))]
-    (->> init
-         (apply (fn [coll1 coll2]
-                  (map pred-fn coll1 coll2)))
-         (every? true?))))
+  "Returns true if a portion of str1 characters can be rearranged
+  to match str2 otherwise returns false."
+  [str1 str2]
+  (->> [str1 str2]
+       (map #(map (frequencies %) (set str2)))
+       (apply #(map gte-and-not-nil? %1 %2))
+       (every? true?)))
 
 
 ;; Benchmarking
@@ -30,16 +29,13 @@
              start#))
         1000000.0)))
 
-
 ;; Main
 
 (defn -main [& args]
   (println "Hello! :)")
-  (let [available-letters (apply str (repeat 1000000 "xzvxvvxffgfdjgopoisehrugidhlkjghlkhjslkjgefasdfsadfwfsadfasfdeasdfasdlfaafsdisaffsdgsdfsdfnadssadsa"))
-        searched-word (apply str (repeat 7000 "ewelina"))
-        execution-time (benchmark
-                         (scramble? available-letters
-                                    searched-word))]
+  (let [available-letters (apply str (repeat 1000000 "xzvxvvxffgfdjgopoisehrugidhlkjghlkhjslkjgefasdfsadfwfsadfasfdeasdfasdlfaafsdisaffsdgsdfsdfnadssadsaessasdasdasdasdisslassssssssewasdasd"))
+        searched-word     (apply str (repeat 7000 "ewelina"))
+        execution-time    (benchmark (scramble? available-letters searched-word))]
     (println (str "Finding " (count searched-word) "-letters word "
                   "in " (count available-letters) " available letters took "
                   execution-time " miliseconds."))))
