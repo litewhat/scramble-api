25cf3660368f84690bd1f23e69b22ddd37b60e1a
Paweł Zielonka <lite.what@gmail.com>
2018-10-14 21:44:38 +0200

    Added logging to scramble endpoint.

--- src/clj/clojure_challenge/services/api/routes/services.clj
+++ src/clj/clojure_challenge/services/api/routes/services.clj
@@ -2,7 +2,7 @@
   (:require [clojure.tools.logging :as log]
             [clojure-challenge.core :refer [scramble?]]
             [compojure.api.sweet :refer :all]
-            [ring.util.http-response :refer :all]))
+            [ring.util.http-response :as response]))
 
 
 (defapi service-routes
@@ -20,7 +20,11 @@
       :body-params [str1 :- String
                     str2 :- String]
       :summary "scramble? FIXME!"
-      (let [response (ok (scramble? str1 str2))]
+      (let [result (scramble? str1 str2)
+            response (if (boolean? result)
+                       (response/ok result)
+                       (response/bad-request result))]
+        (log/info (str "Result: " result))
         (log/info (str "Response: " response))
         response))
 
@@ -29,28 +33,28 @@
       :return Long
       :query-params [x :- Long, {y :- Long 1}]
       :summary "x+y with query-parameters. y defaults to 1."
-      (ok (+ x y)))
+      (response/ok (+ x y)))
 
     (POST "/minus" []
       :return Long
       :body-params [x :- Long, y :- Long]
       :summary "x-y with body-parameters."
-      (ok (- x y)))
+      (response/ok (- x y)))
 
     (GET "/times/:x/:y" []
       :return Long
       :path-params [x :- Long, y :- Long]
       :summary "x*y with path-parameters"
-      (ok (* x y)))
+      (response/ok (* x y)))
 
     (POST "/divide" []
       :return Double
       :form-params [x :- Long, y :- Long]
       :summary "x/y with form-parameters"
-      (ok (/ x y)))
+      (response/ok (/ x y)))
 
     (GET "/power" []
       :return Long
       :header-params [x :- Long, y :- Long]
       :summary "x^y with header-parameters"
-      (ok (long (Math/pow x y))))))
+      (response/ok (long (Math/pow x y))))))
